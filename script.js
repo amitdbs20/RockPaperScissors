39dd8d1ea67855e6d9cbb6f64d69a61570467bce
amitdbs20 <amitdbs20@gmail.com>
2023-01-16 21:18:36 -0500

    Completed RPS game to prompt and return the results in the console tab

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ function getComputerChoice() {
     }
 }
 
+let playerSelectionCase = prompt("ROCK, PAPER, OR SCISSORS?");
+let playerSelection = playerSelectionCase.toUpperCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a draw"
+    }
+    else if ((playerSelection === "ROCK" && computerSelection === "SCISSORS")|| (playerSelection==="PAPER" && computerSelection==="ROCK")||(playerSelection==="SCISSORS"&&computerSelection==="PAPER")) {
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+    } else if ((playerSelection === "ROCK" && computerSelection === "PAPER")|| (playerSelection==="PAPER" && computerSelection==="SCISSORS")||(playerSelection==="SCISSORS"&&computerSelection==="ROCK")) {
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
+    } else {
+        return 'Error. Try again with "ROCK, PAPER, or SCISSORS"'
+    }
+}
